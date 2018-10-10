6c4ac9a475259aa32922254c3da962fb90a61986
Syysmeri, Pekka <pekka.syysmeri@f-secure.com>
2018-10-10 22:33:57 +0300

    cleaned up a little more

--- src/main/java/ht2/database/Database.java
+++ src/main/java/ht2/database/Database.java
@@ -29,22 +29,6 @@ public class Database {
             // Init the db if this is production env (Heroku)
             try {
                 this.initDb();
-                /*
-                List<String> sql = new ArrayList<>();
-                String dataFile = "init-psql.sql";
-                String line = null;
-                
-                FileReader fr = new FileReader(dataFile);
-                BufferedReader br = new BufferedReader(fr);
-                
-                while ((line = br.readLine()) != null) {
-                    System.out.print("line: " + line);
-                    sql.add(line);
-                    System.out.println(" read");
-                }
-                
-                this.addData(sql); */
-                
             } catch (SQLException e) {
                 System.out.println("SQL: " + e.getMessage());
             } catch (FileNotFoundException e) {
@@ -59,78 +43,23 @@ public class Database {
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(address);
     }
-    
-    /*
-    private void initDb() throws SQLException {
-        Connection conn = this.getConnection();
         
-        PreparedStatement stmt
-        PreparedStatement del3 = conn.prepareStatement("DROP TABLE IF EXISTS Vastaus;");
-        del3.execute();
-        del3.close();
-        
-        System.out.println("initDb() Vastaus dropped");
-        
-        PreparedStatement del2 = conn.prepareStatement("DROP TABLE IF EXISTS Kysymys;");
-        del2.execute();
-        del2.close();
-        
-        System.out.println("initDb() Kysymys dropped");
-        
-        PreparedStatement del1 = conn.prepareStatement("DROP TABLE IF EXISTS Kurssi;");
-        del1.execute();
-        del1.close();
-        
-        System.out.println("initDb() Kurssi dropped");
-        
-        PreparedStatement stmt1 = conn.prepareStatement(
-                "CREATE TABLE IF NOT EXISTS Kurssi (id SERIAL PRIMARY KEY,nimi varchar(256) NOT NULL);");
-        stmt1.execute();
-        System.out.println("initDb() Kurssi created");
-        
-        PreparedStatement stmt2 = conn.prepareStatement(
-                "CREATE TABLE IF NOT EXISTS Kysymys (id SERIAL PRIMARY KEY,aihe varchar(256),teksti varchar(1024) NOT NULL,kurssi_id integer NOT NULL,FOREIGN KEY(kurssi_id) REFERENCES Kurssi(id));");
-        stmt2.execute();
-        
-        System.out.println("initDb() Kysymys created");
-        
-        PreparedStatement stmt3 = conn.prepareStatement(
-                "CREATE TABLE IF NOT EXISTS Vastaus (id SERIAL PRIMARY KEY,teksti varchar(1024) NOT NULL,oikein boolean NOT NULL,kysymys_id integer NOT NULL,FOREIGN KEY(kysymys_id) REFERENCES Kysymys(id));");
-        stmt3.execute();
-        
-        System.out.println("initDb() Vastaus created");
-        
-        stmt1.close();
-        stmt2.close();
-        stmt3.close();
-        conn.close();
-    } */
-    
     private void initDb() throws SQLException, FileNotFoundException, IOException {
-        //List<String> sql = new ArrayList<>();
-        String dataFile = "init-psql.sql";
-        //String line = null;
-                
-        FileReader fr = new FileReader(dataFile);
+        FileReader fr = new FileReader("init-psql.sql");
         BufferedReader br = new BufferedReader(fr);
         
         Connection conn = this.getConnection();
         String line;
+        
         while ((line = br.readLine()) != null) {
+            if(line.isEmpty() || line.startsWith("--")) {
+                continue;
+            }
             System.out.print("initDb() - executing: " + line);
-            //sql.add(line);
             conn.createStatement().execute(line);
             System.out.println(" ...OK");
         }
-        /*
-        Connection conn = this.getConnection();
-        for (String s : sql) {
-            System.out.print("addData() " + s);
-            PreparedStatement stmt = conn.prepareStatement(s);
-            stmt.execute();
-            stmt.close();
-            System.out.println(" ...OK");
-        } */
+        
         conn.close();
     }
     
